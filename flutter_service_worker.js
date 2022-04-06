62b18212a7badfbb37bc6d024ec82be5abc9d409
Keke Wu <kwu46@student.monash.edu>
2022-04-06 14:18:51 +0800

    Trying to fix missing pictures

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,8 +31,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ea0356c5ad959ad0ba3fd325f95e69ce",
-"/": "ea0356c5ad959ad0ba3fd325f95e69ce",
+"index.html": "e5f43ffb0d2309eba13ad1e42757875f",
+"/": "e5f43ffb0d2309eba13ad1e42757875f",
 "main.dart.js": "db550d8fa1dca1e7cb73f0ed22653721",
 "manifest.json": "f0dbebcdbf873311c2c3c70e6e111e13",
 "version.json": "1f8373c1f4e85b6217b1268f1a8a2e17"
