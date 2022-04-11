54eee371debb16c69af75014bb99afcd1a02498f
Keke Wu <kwu46@student.monash.edu>
2022-04-11 18:32:37 +0800

    Clean up unused code file

--- android/app/src/main/kotlin/com/example/iteration1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.iteration1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/controllers/controller.dart
+++
@@ -1,5 +0,0 @@
-// import 'package:shorten_my_shower/controllers/menu_controller.dart';
-// import 'package:shorten_my_shower/controllers/navigation_controller.dart';
-
-// MenuController menuController = MenuController.instance;
-// NavigationController navigationController = NavigationController.instance;
