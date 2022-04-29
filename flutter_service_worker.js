9e553eecfccb8dea202857645057232944012d1b
Keke Wu <kwu46@student.monash.edu>
2022-04-29 13:27:29 +0800

    change init state

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -47,9 +47,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4fb9795d97a30fb21818df9899ee0936",
-"/": "4fb9795d97a30fb21818df9899ee0936",
-"main.dart.js": "35168c91538673879edd7e6ab738c6f6",
+"index.html": "f6659c46dad01d1933520b57b7752812",
+"/": "f6659c46dad01d1933520b57b7752812",
+"main.dart.js": "198504ed35213919738b71be1ad0665f",
 "manifest.json": "3318bdcd6229cba3e58d5127ebef2b2f",
 "version.json": "1f8373c1f4e85b6217b1268f1a8a2e17"
 };
