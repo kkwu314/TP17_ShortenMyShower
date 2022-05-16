f78321c90ef84937f65dbcdb3296691f29d0e503
Keke Wu <kwu46@student.monash.edu>
2022-05-16 23:25:51 +0800

    Fix UI according to Mentor feedback

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -63,9 +63,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "335d2ad6ac7f045e6c94d03f8f3f48a4",
-"/": "335d2ad6ac7f045e6c94d03f8f3f48a4",
-"main.dart.js": "cfde697ce1fab8d7f0b2b69ba4e0950d",
+"index.html": "68e8aad893bbb2cf723a1bae86cacebf",
+"/": "68e8aad893bbb2cf723a1bae86cacebf",
+"main.dart.js": "0232d379286001b2f4f3f20247b4ae7b",
 "manifest.json": "3318bdcd6229cba3e58d5127ebef2b2f",
 "version.json": "1f8373c1f4e85b6217b1268f1a8a2e17"
 };
